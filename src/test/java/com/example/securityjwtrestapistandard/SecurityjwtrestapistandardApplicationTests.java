5b1dc7431d64783dc595134b772bae35e55c4fba
s0nnyday <lijnar100@naver.com>
2024-04-01 17:03:22 +0900

    Initial commit

---
+++ src/test/java/com/example/securityjwtrestapistandard/SecurityjwtrestapistandardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityjwtrestapistandard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityjwtrestapistandardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
